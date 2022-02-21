c2e083f31fab027b5c9427189b7886fed8cf24e5
Sovngarded <90443769+Sovngarded@users.noreply.github.com>
2022-02-21 21:48:21 +0700

    Completed entities

---
+++ SibersTest.ApplicationService/DTO/ObjectiveDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SibersTest.ApplicationService.DTO
+{
+    public class ObjectiveDTO
+    {
+        public Guid ObjectiveID { get; set; }
+        public int ProjectID { get; set; }
+        public int ObjectiveAuthorID { get; set; }
+        public int ObjectiveExecutorID { get; set; }
+        public TaskStatus? ObjectiveStatus { get; set; }
+        public string ObjectiveComment { get; set; }
+        public int ObjectivePriority { get; set; }
+        
+    }
+}
--- SibersTest.Domain/Entities/Objective.cs
+++ SibersTest.Domain/Entities/Objective.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SibersTest.Domain.Entities
 {
-    internal class Objective
+    public class Objective
     {
+       [Key] public Guid ObjectiveID { get; set; }
+        public int ProjectID { get; set; }
+        [ForeignKey("ProjectID")]
+        public virtual Objective Project { get; set; }
+        public int ObjectiveAuthorID { get; set; }
+        [ForeignKey("ObjectiveAuthorID")]
+        public virtual Employee EmployeeAuthor { get; set; }
+        public int ObjectiveExecutorID { get; set; }
+        [ForeignKey("ObjectiveExecutorID")]
+        public virtual Employee EmployeeExecutor { get; set; }  
+        public Status? ObjectiveStatus { get; set; }
+        public string ObjectiveComment { get; set; }
+        public int ObjectivePriority { get; set; }
+
+        public enum Status
+        {
+            ToDo, InProgress, Done
+        }
     }
 }
