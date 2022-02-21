8dca8e26d228cd9b3276bcfb116ae94bf9cb4165
Sovngarded <90443769+Sovngarded@users.noreply.github.com>
2022-02-21 20:49:22 +0700

    Added ApplicationService layer

---
+++ SibersTest.ApplicationService/DTO/EmployeeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SibersTest.ApplicationService.DTO
+{
+    public class EmployeeDTO
+    {
+        public Guid EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeMiddleName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public string Email { get; set; }
+    }
---
+++ SibersTest.ApplicationService/Interface/IDataUsage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SibersTest.ApplicationService.Interface
+{
+    public interface IDataUsage<T> where T : class
+    {
+        Task<IEnumerable<T>> Create(T entity);
+        Task Edit(T entityoriginal, T entityfinal);
+        Task Delete(T entity);
+        Task<IEnumerable<T>> GetAll();
+
+
+
+    }
+}
